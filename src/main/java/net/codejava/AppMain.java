545ffa78ee28d5a7565fe9799d87cb96eafba116
IT3 <IT3@IT3-PC>
2020-06-27 11:25:06 +0530

    final commmit ravi

--- src/main/java/net/codejava/AppMain.java
+++ src/main/java/net/codejava/AppMain.java
@@ -2,10 +2,17 @@ package net.codejava;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AppMain {
+public class AppMain  extends SpringBootServletInitializer{
 	public static void main(String[] args) {
 		SpringApplication.run(AppMain.class, args);
 	}
+	
+	  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	        return application.sources(AppMain.class);
+	    }
+	 
 }
